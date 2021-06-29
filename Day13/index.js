3fe992e9f8bf3bd76c599a50b93adc4e24258a6c
Zaheer Abbas <nk4456542@gmail.com>
2021-06-30 00:26:43 +0530

    Fix some bugs

--- Day13/index.js
+++ Day13/index.js
@@ -5,16 +5,20 @@ const nextMemeBtn = document.getElementById("next-meme");
 async function getMeme() {
   meme.innerHTML = "";
   const data = await fetch(RANDOM_MEME_URL);
-  const jsonData = await data.json();
-  const fragment = document.createDocumentFragment();
-  const h3 = document.createElement("h3");
-  h3.textContent = jsonData.title;
-  const img = document.createElement("img");
-  img.src = jsonData.url;
-  img.alt = jsonData.title;
-  fragment.appendChild(h3);
-  fragment.appendChild(img);
-  meme.appendChild(fragment);
+  if (data.status === 200) {
+    const jsonData = await data.json();
+    const fragment = document.createDocumentFragment();
+    const h3 = document.createElement("h3");
+    h3.textContent = jsonData.title;
+    const img = document.createElement("img");
+    img.src = jsonData.url;
+    img.alt = jsonData.title;
+    fragment.appendChild(h3);
+    fragment.appendChild(img);
+    meme.appendChild(fragment);
+  } else {
+    getMeme();
+  }
 }
 
 nextMemeBtn.addEventListener("click", getMeme);
