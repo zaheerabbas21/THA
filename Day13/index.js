b535660c061d1def3c838fbb749a0dc7ed3afdec
Zaheer Abbas <nk4456542@gmail.com>
2021-06-30 00:20:53 +0530

    Day 13 Complete

---
+++ Day13/index.js
@@ -0,0 +1,22 @@
+const RANDOM_MEME_URL = "https://meme-api.herokuapp.com/gimme";
+const meme = document.getElementById("meme");
+const nextMemeBtn = document.getElementById("next-meme");
+
+async function getMeme() {
+  meme.innerHTML = "";
+  const data = await fetch(RANDOM_MEME_URL);
+  const jsonData = await data.json();
+  const fragment = document.createDocumentFragment();
+  const h3 = document.createElement("h3");
+  h3.textContent = jsonData.title;
+  const img = document.createElement("img");
+  img.src = jsonData.url;
+  img.alt = jsonData.title;
+  fragment.appendChild(h3);
+  fragment.appendChild(img);
+  meme.appendChild(fragment);
+}
+
+nextMemeBtn.addEventListener("click", getMeme);
+
+getMeme();
